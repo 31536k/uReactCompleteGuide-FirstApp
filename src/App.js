0e5d6bbe1f05e9c78a0eba46411b5ed02f54f691
kale <kale@kakao.com>
2020-07-07 21:49:32 +0900

    71 Introducing Styled Components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
-import Radium, { StyleRoot } from 'radium';
 
 class App extends Component {
   state = {
@@ -81,18 +80,16 @@ class App extends Component {
 
     // <StyleRoot> Person.js 에서 '@media (min-width: 500px)'을 적용하려면 StyleRoot 로 감싸야 한다. 
     return (
-      <StyleRoot>
-        <div className="App">
-          <h1>Hi, I'm a React App</h1>
-          <p className={classes.join(' ')}>This is really working</p>
-          <button
-            style={style}
-            onClick={this.togglePersonsHandler}>Toggle Persons</button>
-          {persons}
-        </div>
-      </StyleRoot>
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <p className={classes.join(' ')}>This is really working</p>
+        <button
+          style={style}
+          onClick={this.togglePersonsHandler}>Toggle Persons</button>
+        {persons}
+      </div>
     );
   }
 }
 
-export default Radium(App);
+export default App;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,21 +1,35 @@
 import React from 'react';
 import "./Person.css";
-import Radium from 'radium';
+import styled from 'styled-components';
+
+const StyledDiv = styled.div`
+    width: 60%;
+    margin: 16px auto;
+    border: 1px solid #eeeeee;
+    box-shadow: 0 2px 3px #cccccc;
+    padding: 16px;
+    text-align: center;
+
+    @media (min-width: 500px) {
+        width: 450px;
+    }
+`;
 
 const person = (props) => {
     const style = {
-        '@media (min-width: 500px)':  {
+        '@media (min-width: 500px)': {
             width: '450px'
         }
     }
 
-return(
-    <div className="Person" style={style}>
-        <p onClick={props.click}>I'm {props.name}! I'm {props.age} years old</p>
+    return (
+        // <div className="Person" style={style}>
+        <StyledDiv>
+        < p onClick={props.click} > I'm {props.name}! I'm {props.age} years old</p >
         <p>{props.children}</p>
-        <input type = "text" onChange={props.changed} value={props.name}/>
-    </div>
-)
+        <input type="text" onChange={props.changed} value={props.name} />
+        </StyledDiv>
+    )
 }
 
-export default Radium(person);
+export default person;
