6480447446fb73770cd326679f2463baed0f4376
kale <kale@kakao.com>
2020-07-07 21:27:27 +0900

    69 Adding and Using Radium - Style managing tool'

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
-import person from './Person/Person';
+import Radium from 'radium';
 
 class App extends Component {
   state = {
@@ -40,7 +40,11 @@ class App extends Component {
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
     }
 
     let persons = null
@@ -61,6 +65,10 @@ class App extends Component {
       );
 
       style.backgroundColor = 'red';
+      style[':hover'] = {
+        backgroundColor: 'salmon',
+        color: 'black'
+      }
     }
 
     const classes = [];
@@ -84,4 +92,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Radium(App);
