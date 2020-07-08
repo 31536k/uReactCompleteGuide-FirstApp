113d9c4b8b376758b7ebc6d572562cea0bb362e7
kale <kale@kakao.com>
2020-07-08 21:19:42 +0900

    86. A Better Project Structure

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import classes from './App.css';
-import Person from './Person/Person';
-import ErrorBoundary from './ErrorBoundry/ErrorBoundry'
+import Person from '../components/Persons/Person/Person';
 
 class App extends Component {
   state = {
@@ -42,14 +41,13 @@ class App extends Component {
         <div>
           {
             this.state.persons.map((person, index) => {
-              return <ErrorBoundary key={person.id}>
+              return
                 <Person
                   click={() => this.deletePersonsHandler(index)}
                   name={person.name}
                   age={person.age}
                   key={person.id}
                   changed={(event) => this.nameChangedHandler(event, person.id)} />
-              </ErrorBoundary>
             })
           }
         </div>
