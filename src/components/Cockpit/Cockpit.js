f5190b7fbad9a2ad7150f2a04b9ef46ae0692598
kale <kale@kakao.com>
2020-07-09 20:57:29 +0900

    93 Using useEffect() in Functional Components

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,28 +1,33 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import classes from './Cockpit.css'
 
 const cockpit = (props) => {
-    const assignedClasses = [];
-    let buttonClass = '';
+  useEffect(() => {
+    console.log('[Cockpit.js] useEffect')
+    // can fire Http request
+  });
 
-    if (props.showPersons) {
-        buttonClass = classes.Red;
-    }
+  const assignedClasses = [];
+  let buttonClass = '';
 
-    if (props.persons.length <= 2) {
-      assignedClasses.push(classes.red);  // ['red']
-    }
-    if (props.persons.length <= 1) {
-      assignedClasses.push(classes.bold);  // ['red', 'bold']
-    }
+  if (props.showPersons) {
+    buttonClass = classes.Red;
+  }
 
-    return (
-        <div className={classes.Cockpit}>
-            <h1>{props.title}</h1>
-            <p className={assignedClasses.join(' ')}>This is really working</p>
-            <button className={buttonClass} onClick={props.clicked}>Toggle Persons</button>
-        </div>
-    )
+  if (props.persons.length <= 2) {
+    assignedClasses.push(classes.red);  // ['red']
+  }
+  if (props.persons.length <= 1) {
+    assignedClasses.push(classes.bold);  // ['red', 'bold']
+  }
+
+  return (
+    <div className={classes.Cockpit}>
+      <h1>{props.title}</h1>
+      <p className={assignedClasses.join(' ')}>This is really working</p>
+      <button className={buttonClass} onClick={props.clicked}>Toggle Persons</button>
+    </div>
+  )
 }
 
 export default cockpit
