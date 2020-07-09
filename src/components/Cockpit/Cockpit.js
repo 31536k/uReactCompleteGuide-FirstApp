db039ac5d1408a260a3190748465af1c2d51e20b
kale <kale@kakao.com>
2020-07-09 21:15:39 +0900

    95. Cleaning up with Lifecycle Hooks & useEffect

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -7,8 +7,18 @@ const cockpit = (props) => {
     // can fire Http request
     setTimeout(() => {
       console.log('Fake Http rquest')
-    })
+    }, 1000);
+    return () => {
+      console.log('[Cockpit.js] cleanup work in useEffect') // Cockpit component 가 parent 에서 unmount 될 때 호출된다.
+    };
   }, [props.persons]); // props.persons 에 변화가 있을 때만 첫번째 매개변수로 전달한 함수가 실행된다. 빈 array 로 [] 를 주면 최초에 한 번만 실행된다.
+
+  useEffect(() => {
+    console.log('[Cockpit.js] 2nd useEffect');
+    return () => {
+      console.log('[Cockpit.js] cleanup work in 2nd useEffect')
+    };
+  });
 
   const assignedClasses = [];
   let buttonClass = '';
--- src/containers/App.js
+++ src/containers/App.js
@@ -18,7 +18,8 @@ class App extends Component {
       { id: 'id3', name: 'Stephane', age: 26 }
     ],
     otherState: "Some other value",
-    showPersons: false
+    showPersons: false,
+    showCockpit: true
   }
 
   static getDrivedStateFromProps(props, state) {
@@ -28,16 +29,16 @@ class App extends Component {
 
   componentDidMount() {
     // place jobs like http request here
-    console.log('[App.js] componentDidMount'); 
+    console.log('[App.js] componentDidMount');
   }
 
   shouldComponentUpdate(nextProps, nextState) {
-    console.log('[App.js] shouldComponentUpdate'); 
+    console.log('[App.js] shouldComponentUpdate');
     return true;
   }
 
   componentDidUpdate() {
-    console.log('[App.js] componentDidUpdate'); 
+    console.log('[App.js] componentDidUpdate');
   }
 
   nameChangedHandler = (event, id) => {
@@ -65,20 +66,24 @@ class App extends Component {
     let persons = null
 
     if (this.state.showPersons) {
-      persons = <Persons 
-            persons = {this.state.persons}
-            clicked = {this.deletePersonsHandler}
-            changed = {this.nameChangedHandler} />;
+      persons = <Persons
+        persons={this.state.persons}
+        clicked={this.deletePersonsHandler}
+        changed={this.nameChangedHandler} />;
     }
 
     // <StyleRoot> Person.js 에서 '@media (min-width: 500px)'을 적용하려면 StyleRoot 로 감싸야 한다. 
     return (
       <div className={classes.App}>
-        <Cockpit 
+        <button onClick={() => {
+          this.setState({ showCockpit: false }) // cockpit 이 사라지면 Cockpit 의 'cleanup work in useEffect' 부분이 호출된다.
+        }}>Remove cockpit</button>
+
+        {this.state.showCockpit ? <Cockpit
           title={this.props.title}
           showPersons={this.state.showPersons}
           persons={this.state.persons}
-          clicked={this.togglePersonsHandler}/>
+          clicked={this.togglePersonsHandler} /> : null}
         {persons}
       </div>
     );
