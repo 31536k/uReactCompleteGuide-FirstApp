9965e2179867d7ef5aadc4b3e76b2088e6e83778
kale <kale@kakao.com>
2020-07-09 21:50:15 +0900

    102-1. JSX element array instead Root div

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -5,11 +5,11 @@ class Person extends Component {
     render() {
         console.log('[Person.js] rendering...');
         return (
-            <div className={classes.Person}>
-                < p onClick={this.props.click} > I'm {this.props.name}! I'm {this.props.age} years old</p >
-                <p>{this.props.children}</p>
-                <input type="text" onChange={this.props.changed} value={this.props.name} />
-            </div>
+            [
+                <p key="e1" onClick={this.props.click} > I'm {this.props.name}! I'm {this.props.age} years old</p >,
+                <p key="e2">{this.props.children}</p>,
+                <input key="e3" type="text" onChange={this.props.changed} value={this.props.name} />
+            ]
         )
     }
 }
