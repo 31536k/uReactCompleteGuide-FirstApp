3efbf0191353f11d421d21e5524f548b2e238cdd
kale <kale@kakao.com>
2020-07-09 21:56:52 +0900

    102-2. Rendering Adjacent JSX Element. JSX with Aux (only return children) component

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
 import classes from "./Person.css";
+import Aux from '../../../hoc/Aux'
 
 class Person extends Component {
     render() {
         console.log('[Person.js] rendering...');
         return (
-            [
+            <Aux>
                 <p key="e1" onClick={this.props.click} > I'm {this.props.name}! I'm {this.props.age} years old</p >,
                 <p key="e2">{this.props.children}</p>,
                 <input key="e3" type="text" onChange={this.props.changed} value={this.props.name} />
-            ]
-        )
+            </Aux>
+        );
     }
 }
 
